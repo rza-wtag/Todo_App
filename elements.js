9e34a4fd3ca32465f55b9434bb76ef76059bdfd5
rza-wtag <rafi.zaman@welldev.io>
2024-06-09 14:58:58 +0600

    Update elements.js
    Co-authored-by: Irtiza Chowdhury <153156568+ich-wtag@users.noreply.github.com>

--- elements.js
+++ elements.js
@@ -1,4 +1,4 @@
-export const taskForm = document.getElementById("taskForm");
-export const taskTitle = document.getElementById("taskTitle");
-export const taskList = document.getElementById("taskList");
-export const btnCreate = document.querySelector('.btn-create');
+export const $taskForm = document.getElementById("taskForm");
+export const $taskTitle = document.getElementById("taskTitle");
+export const $taskList = document.getElementById("taskList");
+export const $btnCreate = document.querySelector('.btn-create');

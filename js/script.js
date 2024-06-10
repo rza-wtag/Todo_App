4bdf3514d7e08138d2f7f05f3349f0b650225571
rza-wtag <rafi.zaman@welldev.io>
2024-06-10 10:50:37 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,14 +79,6 @@ const createTaskCard = task => {
     return taskCard;
 };
 
-const deleteTask = taskId => {
-    const index = tasks.findIndex(t => t.id === taskId);
-    if (index !== -1) {
-        tasks.splice(index, 1);
-        renderTasks();
-    }
-};
-
 const renderTasks = () => {
     $taskList.innerHTML = "";
 

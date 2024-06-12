00e0eb942e843cda3f6f9e3229ba8511e9c5f3a8
rza-wtag <rafi.zaman@welldev.io>
2024-06-12 08:19:01 +0600

    btnCreate

--- js/elements.js
+++ js/elements.js
@@ -1,4 +1,4 @@
 export const $taskForm = document.getElementById("taskForm");
 export const $taskTitle = document.getElementById("taskTitle");
 export const $taskList = document.getElementById("taskList");
-export const $btnCreate = document.getElementById("btn-create");
+export const $btnCreate = document.getElementById("btnCreate");

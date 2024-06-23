f341fab59131e2270b4d7d4bd8d70bac6d1c6599
rza-wtag <rafi.zaman@welldev.io>
2024-06-23 16:19:46 +0600

    as

--- js/elements.js
+++ js/elements.js
@@ -2,4 +2,3 @@ export const $taskForm = document.getElementById("taskForm");
 export const $taskTitle = document.getElementById("taskTitle");
 export const $taskList = document.getElementById("taskList");
 export const $btnCreate = document.getElementById("btnCreate");
-
